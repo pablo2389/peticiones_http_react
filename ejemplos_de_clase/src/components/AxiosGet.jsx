48c336b8f9df46e621e69362db4ddc745cbc36f6
inoveAlumnos <alumnos@inove.com.ar>
2023-09-05 19:43:26 -0300

    Fix typo

--- ejemplos_de_clase/src/components/AxiosGet.jsx
+++ ejemplos_de_clase/src/components/AxiosGet.jsx
@@ -2,30 +2,31 @@ import { useState } from "react";
 import axios from "axios";
 
 const AxiosGet = () => {
-  const [data, setData] = useState(null)
+  const [data, setData] = useState({});
 
   const getUser = () => {
-    axios.get("https://jsonplaceholder.typicode.com/todos/1")
-    .then((response) => {
-      console.log(response);
-      setData(response.data)
-    })
-    .catch((error) => {
-      console.error(error);
-      alert(`Error al ejecutar la petición: ${error.response.status}`)
-    });
-  }
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos/1")
+      .then((response) => {
+        console.log(response);
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+        alert(`Error al ejecutar la petición: ${error.response.status}`);
+      });
+  };
 
   return (
-    <div style={{maxWidth: "600px", padding: "50px", fontSize: "28px"}}>
+    <div style={{ maxWidth: "600px", padding: "50px", fontSize: "28px" }}>
       <button onClick={() => getUser()}>Obtener datos</button>
-      <ul style={{ textAlign: "start", color: "white"}}>
-        <li>userId: {data? data.userId : "No Data"}</li>
-        <li>id: {data? data.id : "No Data"}</li>
-        <li>title: {data? data.title : "No Data"}</li>
-        <li>completed: {data? String(data.completed) : "No Data"}</li>
+      <ul style={{ textAlign: "start", color: "white" }}>
+        <li>userId: {data.userId ? data.userId : "No data"}</li>
+        <li>id: {data.id ? data.id : "No data "}</li>
+        <li>title: {data.title ? data.title : "No data"}</li>
+        <li>completed: {data.completed ? "Completado" : "Incompleto"}</li>
       </ul>
     </div>
-  )
+  );
 };
 export default AxiosGet;
